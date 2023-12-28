6a5a755f8791030b0b8d37468b77f5a9c5cd8ebb
bilgeulcay <85076456+bilgeulcay@users.noreply.github.com>
2023-12-28 20:45:06 +0300

    filteredRecipe updated

--- lib/FilteredRecipe.dart
+++ lib/FilteredRecipe.dart
@@ -1,4 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
+import 'RecipeDetailsScreen.dart';
+import 'Recipes.dart';
+import 'color.dart';
 
 class FilteredRecipesScreen extends StatelessWidget {
   final List<String> filteredRecipeNames;
@@ -11,14 +16,46 @@ class FilteredRecipesScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Filtered Recipes'),
       ),
-      body: ListView.builder(
+      body: filteredRecipeNames.isEmpty
+          ? Center(
+        child: Text(
+          'No matching recipes found.',
+          style: TextStyle(color: Navy),
+        ),
+      )
+          : ListView.builder(
         itemCount: filteredRecipeNames.length,
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(filteredRecipeNames[index]),
+          final recipeName = filteredRecipeNames[index];
+          return RecipeCard(
+            recipeName: recipeName,
+            imageUrl: 'https://i.pinimg.com/564x/5c/de/08/5cde08f5e2cc6c2493dfbf3f71e7c8c6.jpg',
+            onTap: () {
+              _showRecipeDetails(recipeName);
+            },
           );
         },
       ),
     );
   }
+
+
+  Stream<DocumentSnapshot> _showRecipeDetails(String recipeName) {
+    try {
+    return FirebaseFirestore.instance
+        .collection('recipes')
+        .doc(recipeName)
+        .snapshots()
+        .handleError((error) {
+    // Handle errors here
+    print('Error fetching recipe details: $error');
+    throw StateError('Error fetching recipe details');
+    });
+    } catch (error) {
+    // Handle any errors that occur during the fetch operation
+    print('Error fetching recipe details: $error');
+    throw StateError('Error fetching recipe details');
+    }
+    }
+
 }
